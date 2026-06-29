8a34639bc3d0dfedbb8c9caaea5c5cfa3805e247
Woojin <nthink624@gmail.com>
2026-06-29 10:41:07 +0900

    Force legacy booking route to entry

--- src/app/book/[shopId]/page.tsx
+++ src/app/book/[shopId]/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function BookPage({
   params,
   searchParams,
